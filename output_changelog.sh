16bb9ec61944d9cdd8e21ff2a69fb5cb37859d86
hypercross <helios.tnak@gmail.com>
2013-04-14 20:27:05 -0400

    updated changelog format

--- output_changelog.sh
+++ output_changelog.sh
@@ -1,2 +1,2 @@
-git log --date=short --format="%ad (%d) : %s" > "../../../../../My Box Files/minecraft_publish/Minecraft/changelog.txt" --decorate=short --graph
+git log --date=short --format="%ad%d: %s" > "../../../../../My Box Files/minecraft_publish/Minecraft/changelog.txt" --decorate=short --graph
 
